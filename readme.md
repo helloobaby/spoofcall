2f9dacb6262db64920067a7da5cc901e54e37562
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2023-04-28 14:54:29 +0800

    update

---
+++ readme.md
@@ -0,0 +1,3 @@
+https://medium.com/@fsx30/faking-your-return-address-through-gadget-and-rop-65cc6239599
+
+https://www.unknowncheats.me/forum/anti-cheat-bypass/268039-x64-return-address-spoofing-source-explanation.html
---
+++ spoofcall/spoofcall.cpp
@@ -0,0 +1,29 @@
+﻿/*
+
+jmp_rbx_0 shellcode -> _spoofer_stub.fixup -> test_function -> _spoofer_stub -> MessageBoxA -> jmp_rbx_0 shellcode
+
+*/
+
+
+#include <iostream>
+#include <windows.h>
+#include "spoofcall.h"
+using namespace std;
+
+#pragma section(".text")
+__declspec(allocate(".text")) const unsigned char jmp_rbx_0[] = {0xff, 0x23};//jmp qword ptr[rbx]
+
+int test_function(int k, float p) {
+  int pp = p;
+  return k + pp;
+}
+
+
+int main() {
+  const auto ret = spoof_call(jmp_rbx_0, &test_function, 1, 2.0f);
+  printf("%d\n", ret);
+
+  spoof_call(jmp_rbx_0,&MessageBoxA,(HWND)0,(LPCSTR)"hello",(LPCSTR)"info",(UINT)MB_OK);
+
+  return 0;
+}
